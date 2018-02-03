f29bdc78e039209faf9132e0c0861fc1d5748a2a
dmitrypapka <dmitry.papka@marfeel.com>
2018-02-03 15:28:15 -0300

    #17 Adding basic styles for About section

--- vbarkovskij.com/gulpfile.js
+++ vbarkovskij.com/gulpfile.js
@@ -83,7 +83,7 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['less', 'minify-css', 'minify-js'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
